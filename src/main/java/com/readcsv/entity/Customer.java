f0101dbd79d339c020759415653cde9dcf9dfab7
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2023-10-06 19:43:55 +0600

    getter & setter updated

--- src/main/java/com/readcsv/entity/Customer.java
+++ src/main/java/com/readcsv/entity/Customer.java
@@ -2,24 +2,56 @@ package com.readcsv.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+;
 
 import java.util.Date;
 
-@Getter
-@Setter
+
 @Entity
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
 
     @Column(name = "created")
     private Date created;
     @Column(name = "name")
     private String name;
+
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(int employees) {
+        this.employees = employees;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     @Column(name = "employees")
     private int employees;
     @Column(name = "rating")
